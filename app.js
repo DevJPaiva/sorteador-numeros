81669a5430e34049354a8acfa0da0c0c38cb04ae
DevJPaiva <jorgefe0210@gmail.com>
2025-10-19 18:30:44 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 function sortear() {
-    let quantidade = document.getElementById('quantidade').value;
-    let de = document.getElementById('de').value;
-    let ate = document.getElementById('ate').value;
+  let quantidade = parseInt(document.getElementById("quantidade").value);
+  let de = parseInt(document.getElementById("de").value);
+  let ate = parseInt(document.getElementById("ate").value);
 
-    alert(`quantidade ${quantidade}`);
-    alert(`de ${de}`);
-    alert(`até ${ate}`);
+  let numero = obterNumeroAleatorio(de, ate);
+
+  for (let i = 1; i <= quantidade; i++) {
+    let novoNumero = obterNumeroAleatorio(de, ate);
+
+    // O while repete o sorteio enquanto o número gerado JÁ ESTIVER na lista
+    while (numerosSorteados.includes(novoNumero)) {
+      for (let i = 1; i <= quantidade; i++) {
+        let novoNumero = obterNumeroAleatorio(de, ate);
+
+        // O while repete o sorteio enquanto o número gerado JÁ ESTIVER na lista
+        while (numerosSorteados.includes(novoNumero)) {
+          // Se for repetido, gera um novo número para checar na próxima rodada
+          novoNumero = obterNumeroAleatorio(de, ate);
+        }
+
+        // Adiciona o número único (o while garante que ele não se repete)
+        numerosSorteados.push(novoNumero);
+      }
+      novoNumero = obterNumeroAleatorio(de, ate);
+    }
+
+    numerosSorteados.push(novoNumero);
+  }
 }
+
+function obterNumeroAleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
 
